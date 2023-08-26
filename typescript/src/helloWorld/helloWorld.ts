22304b61da92b3c42bd466e74791fe1c4ae6b268
Gabriel de Freitas Garcia <gabrieldefreitasgarcia@outlook.com>
2023-08-25 23:42:59 -0300

    created hello world in typescript

---
+++ typescript/src/helloWorld/helloWorld.ts
@@ -0,0 +1,6 @@
+// Bem vindos ao desenvolvimento TypeScript!
+// Esse é um exemplo de um arquivo TypeScript, com a extensão .ts
+
+const myName = 'Ciclope';
+
+console.log(`Hello world, ${myName}!`);
